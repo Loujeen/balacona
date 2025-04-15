4a3aa4a7ec0c858de080b63e4244420bdc8453be
Loujeen <loujeenhazem2003@gmail.com>
2025-04-15 23:08:15 +0200

    App_Colors.dart class + empty main class

---
+++ lib/App_Colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color blackColor = Color(0xff242424);
+  static const Color LightButtonColor = Color(0xffF1F6F3);
+  static const Color WhiteColor = Color(0xffffffff);
+  static const Color primaryLightColor = Color(0xffEDFFEB);
+  static const Color primaryDarkColor = Color(0xff036247);
+  static const Color DarkGreyColor = Color(0xffA6A6A6);
+  static const Color LightBlueColor = Color(0xffB7ECF1);
+}
