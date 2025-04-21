2244edfbc430636ef41acc34d78de52cb2f2f52c
Loujeen <loujeenhazem2003@gmail.com>
2025-04-21 22:54:11 +0200

    Finished register screen add images for register screen

---
+++ lib/Register/registerScreen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:testttt/App_Colors.dart';
+import 'package:testttt/Register/register_column.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+  static const String routeName = 'RegisterScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          child:Image.asset("assets/images/register_bg.png",width: double.infinity,height: double.infinity,fit:BoxFit.fill)
+        ),
+      Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation:0,
+          leading: IconButton(
+              onPressed:(){},
+              icon: Icon(Icons.arrow_back_ios_new_rounded ,
+                color: AppColors.primaryDarkColor,)),
+          title: Text(
+            "Balacona",style: TextStyle(
+              fontSize:22, color: AppColors.primaryDarkColor
+              ,fontWeight: FontWeight.w300),),
+          centerTitle: true,),
+
+      body: registerColumn(),)
+      ]
+
+    );
+  }
+}
---
+++ lib/Register/register_column.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:testttt/App_Colors.dart';
+
+class registerColumn extends StatelessWidget {
+  const registerColumn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent, // or any background you want
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+               SizedBox(height:MediaQuery.of(context).size.height*0.001),
+              Text(
+                'Register',
+                style: TextStyle(
+                  color: AppColors.primaryDarkColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50,
+                ),
+              ),
+               SizedBox(height:MediaQuery.of(context).size.height*0.001),
+              Text(
+                'Create your new account',
+                style: TextStyle(
+                  color: AppColors.DarkGreyColor,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(height: 40),
+
+              // Full Name
+              TextField(
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.person),
+                  hintText: 'Full Name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: AppColors.primaryDarkColor)
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(vertical: 20),
+                ),
+              ),
+               SizedBox(height:MediaQuery.of(context).size.height*0.02),
+
+              // Email
+              TextField(
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.email),
+                  hintText: 'Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(color: AppColors.primaryDarkColor)
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(vertical: 20),
+                ),
+              ),
+               SizedBox(height:MediaQuery.of(context).size.height*0.02),
+
+              // Password
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.lock),
+                  hintText: 'Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: AppColors.primaryDarkColor)
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(vertical: 20),
+                ),
+              ),
+               SizedBox(height:MediaQuery.of(context).size.height*0.02),
+
+              // Confirm Password
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.lock),
+                  hintText: 'Confirm Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: AppColors.primaryDarkColor)
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(vertical: 20),
+                ),
+              ),
+               SizedBox(height:MediaQuery.of(context).size.height*0.02),
+              RichText(
+                textAlign: TextAlign.center,
+                text: const TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'By signing you agree to our ',
+                      style: TextStyle(color: AppColors.primaryDarkColor),
+                    ),
+                    TextSpan(
+                      text: 'term of use\nand privacy notice',
+                      style: TextStyle(
+                        color: AppColors.DarkGreyColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+               SizedBox(height:MediaQuery.of(context).size.height*0.01),
+        Image.asset('assets/images/pot.png',height:MediaQuery.of(context).size.height*0.2),
+         SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              backgroundColor: AppColors.primaryDarkColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+            onPressed: () {},
+            child: const Text(
+              'Sign up',
+              style: TextStyle(fontSize: 18,color: AppColors.LightButtonColor),
+            ),
+          ),
+        ),
+         SizedBox(height:MediaQuery.of(context).size.height*0.02),
+
+        // Already have an account? Login
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('already have an account? '),
+            GestureDetector(
+              onTap: () {
+                // Navigate to login screen
+              },
+              child: const Text(
+                'Login',
+                style: TextStyle(
+                  color: AppColors.primaryDarkColor,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+            ),SizedBox(height:MediaQuery.of(context).size.height*0.02 ,)],
+          ),
+        ],)
+      ),
+    ));
+  }
+}
