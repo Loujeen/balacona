0b0a875e87ebaa15877aa00278382225a95a0e64
kmousacodes <139441235+karimamousa@users.noreply.github.com>
2025-06-20 17:12:29 +0300

    Register Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:testttt/Register/registerScreen.dart';
-import 'package:testttt/screens/GameScreen.dart';
-import 'package:testttt/screens/community_screen.dart';
-import 'package:testttt/screens/home_location_screen.dart';
-import 'package:testttt/screens/location_screen.dart';
-import 'package:testttt/screens/login_screen.dart';
+
+import 'firebase_options.dart';
+import 'providers/list_provider.dart';
+import 'providers/auth_user_provider.dart';
+
+import 'Register/registerScreen.dart';
+import 'screens/GameScreen.dart';
+import 'screens/community_screen.dart';
+import 'screens/home_location_screen.dart';
+import 'screens/location_screen.dart';
+import 'screens/login_screen.dart';
 import 'Home_Screen/Home_Screen.dart';
 import 'Home_Screen/LearnAboutPlants.dart';
 import 'Home_Screen/Popularplant.dart';
@@ -15,7 +22,7 @@ import 'LoginSignup.dart';
 import 'Profile/edit_information.dart';
 import 'Profile/profileScreen.dart';
 import 'search_screen.dart';
-import 'firebase_options.dart';
+import 'screens/add_plant/plant_list_tab.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,34 +36,41 @@ void main() async{
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(360, 690),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          initialRoute: GameScreen.routeName,
-          routes: {
-            RegisterScreen.routeName: (context) => RegisterScreen(),
-            HomeScreen.routeName: (context) => HomeScreen(),
-            LoginSignup.routeName: (context) => LoginSignup(),
-            EditInformationScreen.routeName: (context) => EditInformationScreen(),
-            SearchScreen.routeName: (context) => SearchScreen(),
-            KnowAboutPlantsScreen.routeName: (context) => KnowAboutPlantsScreen(),
-            LoginScreen.routeName: (context) => LoginScreen(),
-            LocationScreen.routeName: (context) => LocationScreen(),
-            HomeLocationScreen.routeName: (context) => HomeLocationScreen(),
-            CommunityScreen.routeName: (context) => CommunityScreen(),
-            ProfileScreen.routeName: (context) => ProfileScreen(),
-            LearnAboutPlantScreen.routeName: (context) => LearnAboutPlantScreen(),
-            PopularPlantScreen.routeName: (context) => PopularPlantScreen(),
-            GameScreen.routeName: (context) => GameScreen(),
-
-          },
-        );
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ListProvider()),
+        ChangeNotifierProvider(create: (_) => AuthUserProvider()),
+      ],
+      child: ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            initialRoute: LoginSignup.routeName,
+            routes: {
+              RegisterScreen.routeName: (context) => RegisterScreen(),
+              HomeScreen.routeName: (context) => HomeScreen(),
+              LoginSignup.routeName: (context) => LoginSignup(),
+              EditInformationScreen.routeName: (context) => EditInformationScreen(),
+              SearchScreen.routeName: (context) => SearchScreen(),
+              KnowAboutPlantsScreen.routeName: (context) => KnowAboutPlantsScreen(),
+              LoginScreen.routeName: (context) => LoginScreen(),
+              LocationScreen.routeName: (context) => LocationScreen(),
+              HomeLocationScreen.routeName: (context) => HomeLocationScreen(),
+              CommunityScreen.routeName: (context) => CommunityScreen(),
+              ProfileScreen.routeName: (context) => ProfileScreen(),
+              LearnAboutPlantScreen.routeName: (context) => LearnAboutPlantScreen(),
+              PopularPlantScreen.routeName: (context) => PopularPlantScreen(),
+              GameScreen.routeName: (context) => GameScreen(),
+              TaskListTab.routeName: (context) => TaskListTab(),
+            },
+          );
+        },
+      ),
     );
   }
 }
